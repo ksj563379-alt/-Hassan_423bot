7dfd34608d73e69eeaf41f86a2d98ca26fabff3e
ksj563379-alt <ksj563379@gmail.com>
2026-08-01 17:06:54 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-print("Test")
+import asyncio
+import logging
+from aiogram import Bot, Dispatcher, types, F
+from aiogram.utils.keyboard import InlineKeyboardBuilder
+
+TOKEN = "7820150993:AAEQw_Fbbg72Xdf5gN00bX6ksj5633"
+
+bot = Bot(token=TOKEN)
+dp = Dispatcher()
