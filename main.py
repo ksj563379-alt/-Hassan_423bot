064d0985d9779121487b12abc271009e31b2a5fa
ksj563379-alt <ksj563379@gmail.com>
2026-08-01 17:19:46 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import logging
 from aiogram import Bot, Dispatcher, types, F
 from aiogram.utils.keyboard import InlineKeyboardBuilder
 
-TOKEN = "7820150993:AAEQw_Fbbg72Xdf5gN00bX6ksj5633"
+TOKEN = "8670994653:AAFOmRKRGkaPmAG9Lccs9YCapDyivGp1YZU"
 
 bot = Bot(token=TOKEN)
 dp = Dispatcher()
