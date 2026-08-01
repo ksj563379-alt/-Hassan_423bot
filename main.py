a4d4008176bce1d3482b48bbcd5059593ddf8df4
ksj563379-alt <ksj563379@gmail.com>
2026-08-01 17:12:21 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,3 +2,5 @@ import asyncio
 import logging
 from aiogram import Bot, Dispatcher, types, F
 from aiogram.utils.keyboard import InlineKeyboardBuilder
+
+TOKEN = "7820150993:AAEQw_Fbbg72Xdf5gN00bX6ksj5633"
